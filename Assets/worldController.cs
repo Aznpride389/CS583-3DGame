9ccea1f5e85d4c8e8cd573c64ada7a38397700b8
Jose Carlos Gomez <josecarlosgomez69gmail.com>
2018-12-08 15:36:02 -0800

    Worked on worldController and tilting. Only tilts in one direction

---
+++ Assets/worldController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class worldController : MonoBehaviour {
+
+    public float TiltX = 0f;
+    public float TiltY = 0f;
+
+    //User can control rotation of player
+    void FixedUpdate()
+    {
+        TiltX += Input.GetAxis("Horizontal");
+        transform.eulerAngles = new Vector3(5, 0, TiltX);
+
+        TiltY += Input.GetAxis("Vertical");
+        transform.eulerAngles = new Vector3(5, TiltY, 0);
+
+    }
+
+
+}
+

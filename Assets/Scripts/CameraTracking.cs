3a71a0b20fc6959ce55cd782d4198e3bc2bc19fb
Kevin Rogers <rogersk.513@gmail.com>
2018-12-08 16:42:14 -0800

    Duping Carlos's work into my directory

--- Assets/Scripts/CameraTracking.cs
+++ Assets/Scripts/CameraTracking.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraTracking : MonoBehaviour {
 
+    public GameObject player;
+
 	// Use this for initialization
 	void Start () {
 		
---
+++ Assets/playerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    //private Rigidbody2D rb2d;
+
+    public Rigidbody rb;
+
+    public float speed = 5.0f;
+
+    //User can control rotation of player
+    void Update()
+    {
+        // Tilts player with arrow keys as determined in Input settings
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+}

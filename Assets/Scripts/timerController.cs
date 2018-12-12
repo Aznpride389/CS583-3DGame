e1fd7bd07d1736b5b55eeed39cd4b6e6b8adcf7d
Kevin Rogers <rogersk.513@gmail.com>
2018-12-11 21:11:39 -0800

    added minimap

---
+++ Assets/Scripts/timerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class timerController : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject miniCam;
+    public GameObject miniMap;
+    public float speed = 15.0f;
+
+    public Text timeText;
+    public float time;
+
+    private Vector3 cur = Vector3.zero;
+
+    // Use this for initialization
+    void Start () {
+        time = 30;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        time -= Time.deltaTime;
+        timeText.text = time.ToString("0.0");
+
+        miniCam.transform.position = Vector3.SmoothDamp(miniCam.transform.position, (player.transform.position + new Vector3(0, 10, 0)), ref cur, .01f, speed);
+        //miniCam.transform.LookAt(player.transform);
+    }
+}

da03cd3b841b726ec6273987481653caa102a94d
Jose Carlos Gomez <josecarlosgomez69gmail.com>
2018-12-11 18:10:25 -0800

    Added canvas with simple timer

---
+++ Assets/Scripts/timerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class timerController : MonoBehaviour {
+
+    public float time;
+    public Text timeText;
+	// Use this for initialization
+	void Start () {
+        time = 0;
+        
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        time += Time.deltaTime;
+        timeText.text = time.ToString("0.0");
+	}
+}

dde9908d76bb9f39ab7273296e96f85fd803c890
James Zhang <zhangjames389@gmail.com>
2018-12-16 17:03:01 -0800

    Main Menu Scene w/ Gfx and buttons

---
+++ Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadOnClick : MonoBehaviour
+{
+    public void LoadScene(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void TaskOnClick()
+    {
+        Application.Quit();
+    }
+    
+}

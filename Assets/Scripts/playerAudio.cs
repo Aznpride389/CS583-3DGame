0e6021d14b2afc1cda6216f1eea96f2a25dfaa4f
Kevin Rogers <rogersk.513@gmail.com>
2018-12-18 19:27:17 -0800

    added bounce animation on falloff

---
+++ Assets/Scripts/playerAudio.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAudio : MonoBehaviour {
+
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+}
